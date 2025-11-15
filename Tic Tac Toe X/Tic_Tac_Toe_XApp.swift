a569810286b8e6deab1edf7fdea1320b88cbee49
Stephanie Rocha M <srochamarquez@bsd220.org>
2025-11-15 13:03:54 -0600

    Initial Commit

---
+++ Tic Tac Toe X/Tic_Tac_Toe_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_XApp.swift
+//  Tic Tac Toe X
+//
+//  Created by stephanie rocha marquez on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
