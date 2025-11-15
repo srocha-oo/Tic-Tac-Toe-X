dbf46d7c7afa1646ec8864e2cabe498d06a95b6f
Stephanie Rocha M <srochamarquez@bsd220.org>
2025-11-15 15:24:54 -0600

    Added rotation animation

--- Tic Tac Toe X/ContentView.swift
+++ Tic Tac Toe X/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
+                        Color.white
+                            .opacity(moves[index] == "" ? 1 : 0)
                         Text(moves[index])
                             .font(.system(size: 60))
                             .foregroundColor(.white)
@@ -35,7 +37,7 @@ struct ContentView: View {
                             }
                         }
                     }
-                    
+                    .rotation3DEffect(.degrees(moves[index] == "" ? 180 : 0), axis: (x: 0, y: 1, z: 0))
                 }
             }
         }
