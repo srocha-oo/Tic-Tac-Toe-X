6edb499f2d92b2c7b2ddb03b86724be45efb5816
Stephanie Rocha M <srochamarquez@bsd220.org>
2025-11-20 22:09:57 -0600

    Added 'Cat's Game' determination

--- Tic Tac Toe X/ContentView.swift
+++ Tic Tac Toe X/ContentView.swift
@@ -17,9 +17,7 @@ struct ContentView: View {
             Text("Tic Tac Toe")
                 .font(.largeTitle)
                 .bold()
-            LazyVGrid(
-                columns: Array(repeating: GridItem(.fixed(120)), count: 3)
-            ) {
+            LazyVGrid(columns: Array(repeating: GridItem(.fixed(120)), count: 3)) {
                 ForEach(0..<9) { index in
                     ZStack {
                         Color.blue
@@ -45,7 +43,13 @@ struct ContentView: View {
         }
         .preferredColorScheme(.dark)
         .alert(isPresented: $gameOver) {
-            Alert(title: Text(winMessage))
+            Alert(title: Text(winMessage), dismissButton: .destructive(Text("Play Again"),
+                action: {
+                    withAnimation {
+                        moves = Array(repeating: "", count: 9)
+                        gameOver = false
+                    }
+            }))
         }
         .onChange(of: moves) { oldValue, newValue in
             checkForWinner()
@@ -54,22 +58,24 @@ struct ContentView: View {
 
     private func checkForWinner() {
         checkLine(a: 0, b: 1, c: 2)
-           checkLine(a: 3, b: 4, c: 5)
-           checkLine(a: 6, b: 7, c: 8)
-           checkLine(a: 0, b: 4, c: 8)
-           checkLine(a: 6, b: 4, c: 2)
+        checkLine(a: 3, b: 4, c: 5)
+        checkLine(a: 6, b: 7, c: 8)
+        checkLine(a: 0, b: 4, c: 8)
+        checkLine(a: 6, b: 4, c: 2)
+        checkLine(a: 0, b: 3, c: 6)
+        checkLine(a: 1, b: 4, c: 7)
+        checkLine(a: 2, b: 5, c: 8)
            if !(gameOver || moves.contains("")) {
                winMessage = "Cat's Game!"
                gameOver = true
-
-        }
+           }
     }
     private func checkLine(a: Int, b: Int, c: Int) {
         if moves[a] != "" && moves[a] == moves[b] && moves[b] == moves[c] {
-                   winMessage = "\(moves[a]) is winner!"
-                   gameOver = true
-               }
+            winMessage = "\(moves[a]) is the winner!"
+            gameOver = true
         }
+    }
 }
 #Preview {
     ContentView()
